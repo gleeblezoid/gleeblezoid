085d39287bd8bacf6dc440f45230fb2769a85ce3
Urse <gleeblezoid@gmail.com>
2021-11-30 21:39:17 +0000

    Update setup.sh

--- Tinkering projects/Automac/setup.sh
+++ Tinkering projects/Automac/setup.sh
@@ -23,6 +23,7 @@ rm -rf MacPorts-2.7.1*
 echo "install xcode full"
 sudo port install mas
 mas lucky xcode
+mas lucky bitwarden
 
 echo "Install ports from list"
 sudo curl --location --remote-name https://github.com/macports/macports-contrib/raw/master/restore_ports/restore_ports.tcl
